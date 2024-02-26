135e886c4c918ce41e1ce690b6e2c34f8e49fa1b
tgonzalezdiuk <tamara.gsx@gmail.com>
2024-02-26 15:00:15 -0500

    15:00 - 2/26/24

---
+++ Circle.java
@@ -0,0 +1,21 @@
+/*************
+ * Tamara Gonzalez
+ * Date: Febuary 26th, 2024
+ * Program Name: Java
+ ***************/
+
+import java.io.*;
+public class Circle
+{
+    public static void main (String[ ] args)
+    {
+        double radius = 4.28;
+        double area = 0;
+        double circumference;
+        area = (3.14)*(radius)*(radius);
+        circumference = 2*3.14*radius;
+        System.out.println("The area of the circumference " + area);
+        System.out.println("The circumference of the circle is " + radius);
+    }
+}
+
---
+++ Rectangle.java
@@ -0,0 +1,15 @@
+/*************
+ * Tamara Gonzalez
+ * Date: Febuary 26th, 2024
+ * Program Name: Java
+ ***************/
+
+import java.io.*;
+public class Rectangle
+{
+    public static void main (String[ ] args)
+    {
+        System.out.println(" ***************\n*\t\t*\n*\t\t*\n*\t\t*\n*\t\t*\n*\t\t*\n ***************");
+    }
+}
+
