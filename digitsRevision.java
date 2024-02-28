a741b9b8c483cc99d3456d57eca8cb78ab7f0dc0
tgonzalezdiuk <tamara.gsx@gmail.com>
2024-02-28 15:00:29 -0500

    15:00 2/28/24

---
+++ digitsRevision.java
@@ -0,0 +1,31 @@
+/*************
+ * Tamara Gonzalez
+ * Date: Febuary 28th, 2024
+ * Program Name: Java
+ ***************/
+
+ import java.util.*;
+
+ public class digitsRevision 
+ {
+     public static void main (String [] args)
+     {
+         int hundreds;
+         int tens;
+         int ones;
+        int num
+        
+        Scanner input = new Scanner(system.in);
+        
+        hundreds = num/100;
+        tens = (num%100)/10 ;
+        ones = num%10;
+        
+        System.out.print("Enter a three digit number ");
+        System.out.println("The hundreds-place digit is: " + hundreds);
+        System.out.println("The tens-place digit is: " + tens);
+        System.out.println("The one's digit is: " + ones);
+
+     num = input.nextInt();
+    }
+}
