d0c19529f57a72bdf9a36b4aa71a8179143a1e6c
tgonzalezdiuk <tamara.gsx@gmail.com>
2024-03-01 14:38:40 -0500

    14:38 03/01/24

--- DivandMod.java
+++ DivandMod.java
@@ -4,7 +4,18 @@
  * Program Name: Java
  ***************/
 
+import java.util.*;
+
 public class DivandMod 
 {
     public static void main (String[ ] args)
     {
+        Scanner input = new Scanner(System.in);
+     
+         System.out.print("Enter an integer ");
+         System.out.print("Enter a second integer ")
+
+         l 
+
+
+
